bb818ebeb8be57f58f490addb435e20ba526bce4
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-09-15 17:51:12 +0300

    final task

---
+++ Final_task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+void ToNewArray(string[] array, string[] newArray)
+{
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] array = { "1234", "1567", "-2", "computer science", "good", "one" };
+Console.WriteLine(string.Join(", ", array));
+string[] newArray = new string[6];
+ToNewArray(array, newArray);
+PrintArray(newArray);
